5d5dcbd2e7fa75cb4b163c23f03aca7f661dfb88
Brad Gibson <b2b@humanenginuity.com>
2018-09-17 22:49:22 -0700

    game_score_nothing_is_valid: red

--- src/unit_tests/game.rs
+++ src/unit_tests/game.rs
@@ -1,6 +1,20 @@
 use game::Game;
 
 #[test]
+fn game_score_nothing_is_valid() {
+    // given a game validate function
+    let rolls = &[];
+    let expected_result = Result::Ok(&[]);
+    let validate = Game::validate;
+
+    // when an empty slice is validated
+    let result = validate(rolls);
+
+    // then the result should be OK
+    assert_eq!(result, expected_result);
+}
+
+#[test]
 fn game_score_nothing_yields_none() {
     // given a game score function
     let rolls = &[];
